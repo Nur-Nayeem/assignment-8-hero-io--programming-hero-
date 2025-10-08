49161eb754100f5cc1d9506893c8bbea300257b9
nur-nayeem <nurnayem768@gmail.com>
2025-10-08 08:55:11 +0600

    modify footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,36 +12,55 @@ const Footer = () => {
       <Link to="/">Home</Link>
       <Link to="/apps">Apps</Link>
       <Link to="/installed">Installation</Link>
+      <Link to="#">About Us</Link>
+      <Link to="#">Contact</Link>
+      <Link to="#">FAQs</Link>
+      <Link to="#">Privacy Policy</Link>
     </>
   );
   return (
-    <div className="flex flex-col items-center justify-center bg-black text-white py-20 text-center">
+    <div className="flex flex-col items-center justify-center bg-black text-white pt-20 pb-10 text-center px-2.5">
       <Link to="/" className="flex items-center gap-1">
         <figure className="w-7 h-7  md:w-8 md:h-8">
           <img className="w-full h-full" src={logo} alt="logo" />
         </figure>
         <h1 className="text-lg gradient-text font-bold">LOGO.IO</h1>
       </Link>
-      <p className="my-5">
-        Commodi quasi praesentium a alias porro facere veniam inventore iste,
-        corporis labore, similique id, fuga est officiis totam.
+      <p className="my-5 max-w-3xl">
+        Enjoy the full power of our platform in your pocket. Our mobile app
+        offers a smoother, faster, and more personalized experience, helping you
+        get more done on the go.
       </p>
-      <div className="flex items-center justify-center gap-10 mb-5">
-        <div className="bg-white w-8 h-8 rounded-full flex items-center justify-center text-black">
+      <div className="flex items-center justify-center gap-10 flex-wrap mb-5">
+        <a
+          href="#"
+          className="bg-white w-8 h-8 rounded-full flex items-center justify-center text-black"
+        >
           <FaLinkedinIn />
-        </div>
-        <div className="bg-white w-8 h-8 rounded-full flex items-center justify-center text-black">
+        </a>
+        <a
+          href="#"
+          className="bg-white w-8 h-8 rounded-full flex items-center justify-center text-black"
+        >
           <FaFacebookF />
-        </div>
-        <div className="bg-white w-8 h-8 rounded-full flex items-center justify-center text-black">
+        </a>
+        <a
+          href="#"
+          className="bg-white w-8 h-8 rounded-full flex items-center justify-center text-black"
+        >
           <FaXTwitter />
-        </div>
-        <div className="bg-white w-8 h-8 rounded-full flex items-center justify-center text-black">
+        </a>
+        <a
+          href="#"
+          className="bg-white w-8 h-8 rounded-full flex items-center justify-center text-black"
+        >
           <RiInstagramFill />
-        </div>
+        </a>
       </div>
-      <div className="flex items-center justify-center gap-10">{links}</div>
-      <hr className="w-full h-2 text-[#001931] my-4" />
+      <div className="flex items-center justify-center gap-10 flex-wrap">
+        {links}
+      </div>
+      <hr className="w-full h-2 text-[#001931] mt-8 mb-2" />
       <p>Copyright © 2025 - All right reserved</p>
     </div>
   );
