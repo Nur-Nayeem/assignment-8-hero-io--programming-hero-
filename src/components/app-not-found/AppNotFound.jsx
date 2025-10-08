52a304ad2e90e0e174e03a3cc2121a0e8c89a4cb
nur-nayeem <nurnayem768@gmail.com>
2025-10-08 10:13:54 +0600

    added page not found and app not found components

---
+++ src/components/app-not-found/AppNotFound.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import appNotFound from "../../assets/App-Error.png";
+import { Link } from "react-router";
+
+const AppNotFound = () => {
+  return (
+    <div className="flex flex-col justify-center items-center py-20 text-center px-2.5">
+      <figure>
+        <img src={appNotFound} alt="Not Found" />
+      </figure>
+      <h2 className="text-5xl font-bold text-[#001931]">
+        Oops, page not found!
+      </h2>
+      <p className="my-4 text-xl text-[#627382]">
+        The page you are looking for is not available.
+      </p>
+      <Link to={-1} className="w-max mx-auto ">
+        <button className="cursor-pointer font-semibold bg-gradient-to-r from-[#632ee3] to-[#9f62f2] py-3.5 px-10 text-white rounded-md">
+          Go Back!
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default AppNotFound;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import App from "../App";
 import Home from "../pages/Home";
 import Apps from "../pages/Apps";
+import NotFound from "../pages/NotFound";
 
 export const router = createBrowserRouter([
   {
@@ -19,6 +20,10 @@ export const router = createBrowserRouter([
       {
         path: "installed",
         element: <h1>Installed</h1>,
+      },
+      {
+        path: "*",
+        Component: NotFound,
       },
     ],
   },
